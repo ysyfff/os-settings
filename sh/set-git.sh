2b00bb81fd20c2f114145b3989f35f569dc9bacc
ysyfff <1156546473@qq.com>
2014-01-19 11:21:52 +0800

    complete set-git.sh

---
+++ sh/set-git.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+git config --global user.name "ysyfff"
+git config --global user.email "1156546473@qq.com"
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=360000000'
