293cc6d1ad0d7ea1417f2523eb925e97d84feb4b
ysyfff <1156546473@qq.com>
2014-01-21 00:53:03 +0800

    add nodejs

--- sh/ppa-os.sh
+++ sh/ppa-os.sh
@@ -2,7 +2,18 @@
 
 
 sudo add-apt-repository ppa:webupd8team/sublime-text-2
+echo '>>>>>>>>>>>>>>>>>>>> sublime was added! <<<<<<<<<<<<<<<<<<<<'
+
+sudo apt-get install python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+echo '>>>>>>>>>>>>>>>>>>>> nodejs was added! <<<<<<<<<<<<<<<<<<<<'
+
 sudo apt-get update
+echo '>>>>>>>>>>>>>>>>>>>> update was done! <<<<<<<<<<<<<<<<<<<<'
+
 sudo apt-get install sublime-text
-echo '>>>>>>>>>>>>>>>>>>>> sublime was done! <<<<<<<<<<<<<<<<<<<<'
+echo '>>>>>>>>>>>>>>>>>>>> nodejs was done! <<<<<<<<<<<<<<<<<<<<'
+
+sudo apt-get install nodejs
+echo '>>>>>>>>>>>>>>>>>>>> nodejs was done! <<<<<<<<<<<<<<<<<<<<'
 
